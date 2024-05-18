2c596148fd1ea6623863f968b9c0e4a2842272ce
jtrrll <jacksonterrill3@gmail.com>
2024-05-18 14:46:46 -0400

    refactor: remove ability for scripts to read stdin
    prefer using xargs in most scenarios. also we are passing shell check now

--- scripts/install_nix.sh
+++ scripts/install_nix.sh
@@ -9,15 +9,12 @@ install_nix() {
 case "$(./"$(dirname "$0")"/get_os.sh 2> /dev/null)" in
   "LINUX")
     printf "Detected OS: Linux\n"
-    install_nix
     ;;
   "WSL")
     printf "Detected OS: WSL\n"
-    install_nix
     ;;
   "MACOS")
     printf "Detected OS: macOS\n"
-    install_nix
     ;;
   *)
     printf "\033[31;1merror\033[0m: unsupported OS\n"
@@ -25,7 +22,7 @@ case "$(./"$(dirname "$0")"/get_os.sh 2> /dev/null)" in
     ;;
 esac
 
-if [ $? -eq 0 ]; then
+if install_nix; then
   printf "\033[32;1mNix installation successful\033[0m\n"
   exit 0
 else
