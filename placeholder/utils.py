0b65dde856a9e10acdfb3953e840599b7fc2e2b4
aaronkimbrooktec <218adk@gmail.com>
2024-05-22 00:55:07 -0400

    fix: change path field to Path object

--- placeholder/utils.py
+++ placeholder/utils.py
@@ -1,11 +1,13 @@
+from pathlib import Path
+
 class File:
     def __init__(self, path: str, content: str = None) -> None:
-        self.path: str = path
+        self.path: Path = Path(path)
         self.content: str = content
 
 class Directory:
     def __init__(self, path: str) -> None:
-        self.path: str = path
+        self.path: Path = Path(path)
 
 class ValidationError(Exception):
     pass
