6a557b5ab1304054cb9ab00c08819f7717994597
Aaron Kim <44210329+AaronKim218@users.noreply.github.com>
2024-06-03 09:57:15 -0400

    Merge gather reqs cli with initialize repo cli (#21)
    Combined muneer's cli with mine with some modifications. removed all
    args so cli only supports interactive flow. added custom inquirer theme
    so color scheme is more consistent

--- cli/cli.py
+++ cli/cli.py
@@ -1,30 +1,46 @@
 import os
+import sys
 from typing import Callable
+import inquirer
 import typer
 from pathlib import Path
 from rich.console import Console
 from rich.prompt import Confirm, Prompt
 from templates import get_flake, MIT_LICENSE, PULL_REQUEST_TEMPLATE
-from utils import Directory, File, ValidationError
+from utils import Directory, File, ValidationError, CustomTheme
 
-app = typer.Typer()
+app = typer.Typer(add_completion=False)
 stdout_console = Console()
 stderr_console = Console(stderr=True)
 
-startup_art = """
-   _____  ____________
-  /  _/ |/ /  _/_  __/
- _/ //    // /  / /   
-/___/_/|_/___/ /_/    
+BACKEND_LANGUAGES = ['Python', 'Java', 'C++', 'Rust', 'Go', 'None']
+FRONTEND_LANGUAGES = ['JavaScript', 'TypeScript', 'None']
+DATABASES = ['PostgreSQL', 'MySQL', 'SQLite', 'MongoDB', 'None']
 
-"""
-stdout_console.print(f"[bold dodger_blue1]{startup_art}[/bold dodger_blue1]")
+def select_pkgs() -> str:
+    bold_cyan_code = '\033[1;96m'
+    reset_code = '\033[0m'
 
-def parse_pkgs(val: str) -> list[str]:
-    stripped_val = val.strip()
-    if not stripped_val:
-        raise ValidationError("Packages cannot be empty.")
-    return stripped_val.split()
+    try:
+        questions = [
+            inquirer.List('backend',
+                message=f"{bold_cyan_code}Select the backend language{reset_code}",
+                choices=BACKEND_LANGUAGES
+            ),
+            inquirer.List('frontend',
+                message=f"{bold_cyan_code}Select the frontend language{reset_code}",
+                choices=FRONTEND_LANGUAGES
+            ),
+            inquirer.List('database',
+                message=f"{bold_cyan_code}Select the database{reset_code}",
+                choices=DATABASES
+            )
+        ]
+        answers = inquirer.prompt(questions, theme=CustomTheme(), raise_keyboard_interrupt=True)
+        return [pkg for pkg in [answers['backend'], answers['frontend'], answers['database']] if pkg != 'None']
+    except KeyboardInterrupt:
+        stderr_console.print("[red]Aborted.[/red]")
+        sys.exit(1)
 
 def parse_name(val: str) -> str:
     stripped_val = val.strip()
@@ -61,23 +77,25 @@ def create_files(files: list[File], base_path: Path) -> None:
         file_path.write_text(file.content.strip() + "\n" if file.content else "")
 
 @app.command()
-def create_repo(
-    pkgs: str = typer.Option(
-        ...,
-        help="List of packages to include in the flake.nix file"
-    )
-) -> None:
-    
-    # parse input arguments
-    pkgs = parse_pkgs(pkgs)
+def create_repo() -> None:
+    startup_art = """
+   _____  ____________
+  /  _/ |/ /  _/_  __/
+ _/ //    // /  / /   
+/___/_/|_/___/ /_/    
+"""
+    stdout_console.print(f"[bold dodger_blue1]{startup_art}[/bold dodger_blue1]")
 
     # prompt user for repository name and path
     name = prompt_and_parse("Enter the name of the repository", parse_name)
+
+    PKGS = select_pkgs()
+
     if Confirm.ask("[bold cyan]Do you want to create the repository in the current directory?[/bold cyan]"):
         path = Path(os.getcwd())
     else:
         path = prompt_and_parse("Enter the path to the repository", parse_path)
-    
+
     # define base path and create root directory
     BASE_PATH = path / name
     BASE_PATH.mkdir(parents=True, exist_ok=True)
@@ -85,7 +103,7 @@ def create_repo(
     # list of predefined objects to create
     objects = [
         # root files
-        File("flake.nix", content=get_flake(pkgs)),
+        File("flake.nix", content=get_flake(PKGS)),
         File("LICENSE", content=MIT_LICENSE),
         File(".gitignore"),
         File("README.md"),
