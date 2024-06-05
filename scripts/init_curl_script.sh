2d55445a0fe6665bbde8fb0a92d41ebc5e42a940
Muneer Lalji <muneerlalji@gmail.com>
2024-06-05 16:52:43 -0400

    realized i needed to run it directly through the users shell insteads of through the curl shell

--- scripts/init_curl_script.sh
+++ scripts/init_curl_script.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
 
-# Define the base URL for the GitHub repository
+# base URL for raw files
 GITHUB_REPO_URL="https://raw.githubusercontent.com/GenerateNU/init/main"
 GITHUB_NEW_URL="https://raw.githubusercontent.com/GenerateNU/init/create-executable"
 
-# Define the script URLs
+# Full URLs
 GET_OS_URL="$GITHUB_REPO_URL/scripts/get_os.sh"
 INSTALL_NIX_URL="$GITHUB_REPO_URL/scripts/install_nix.sh"
 CLI_URL="$GITHUB_NEW_URL/cli/cli.py"
 TEMPLATES_URL="$GITHUB_NEW_URL/cli/templates.py"
 UTILS_URL="$GITHUB_NEW_URL/cli/utils.py"
 
+# Make sure command exits if error detected
 check_exit_status() {
   if [ $? -ne 0 ]; then
     exit 1
@@ -18,12 +19,12 @@ check_exit_status() {
 }
 
 # Download and run the Nix installation script
-# curl -L $GET_OS_URL -o get_os.sh 
-# curl -L $INSTALL_NIX_URL -o install-nix.sh
-# chmod +x get_os.sh
-# chmod +x install-nix.sh
-# ./install-nix.sh
-# check_exit_status
+curl -L $GET_OS_URL -o get_os.sh 
+curl -L $INSTALL_NIX_URL -o install-nix.sh
+chmod +x get_os.sh
+chmod +x install-nix.sh
+./install-nix.sh
+check_exit_status
 
 # Download scripts to run cli
 curl -L $UTILS_URL -o utils.py
@@ -32,9 +33,9 @@ curl -L $CLI_URL -o cli.py
 chmod +x cli.py
 ./cli.py
 
-# Clean up the downloaded files
-# rm install-nix.sh
-# rm get_os.sh
+# Clean up files
+rm install-nix.sh
+rm get_os.sh
 rm cli.py
 rm utils.py
 rm templates.py
