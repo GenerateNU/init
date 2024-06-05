dd358faad70116598cc2d446bf1a64def8cb283f
Muneer Lalji <muneerlalji@gmail.com>
2024-06-05 09:00:18 -0400

    shebang for running script through nix shell

--- cli/cli.py
+++ cli/cli.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i python3 -p python3Packages.typing python3Packages.inquirer python3Packages.typer python3Packages.rich
+
 import os
 import sys
 from typing import Callable
