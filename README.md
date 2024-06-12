d7b09eaf2a87e90e1779deb09c21eb47a6f978c2
Muneer Lalji <121840660+muneerlalji@users.noreply.github.com>
2024-06-12 19:30:32 -0400

    One click install (#29)
    allows user to run the install nix script as well as the cli using the
    command specified in the readme.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # init
 Generating reproducible development environments
+
+## Running init
+
+To quickly set up your repository and development environment:
+
+1. Open your terminal.
+2. Run the following command to download nix and run the cli script to create your repo:
+
+   ```console
+   curl -L https://raw.githubusercontent.com/GenerateNU/init/one-click-install/scripts/init_curl_script.sh -o init_curl_script.sh && chmod +x init_curl_script.sh && ./init_curl_script.sh ; rm init_curl_script.sh
---
+++ scripts/init_curl_script.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+# base URL for raw files
+GITHUB_REPO_URL="https://raw.githubusercontent.com/GenerateNU/init/main"
+GITHUB_NEW_URL="https://raw.githubusercontent.com/GenerateNU/init/create-executable"
+
+# Full URLs
+GET_OS_URL="$GITHUB_REPO_URL/scripts/get_os.sh"
+INSTALL_NIX_URL="$GITHUB_REPO_URL/scripts/install_nix.sh"
+CLI_URL="$GITHUB_NEW_URL/cli/cli.py"
+TEMPLATES_URL="$GITHUB_NEW_URL/cli/templates.py"
+UTILS_URL="$GITHUB_NEW_URL/cli/utils.py"
+
+# Make sure command exits if error detected
+check_exit_status() {
+  if [ $? -ne 0 ]; then
+    rm install_nix.sh
+    rm get_os.sh
+    exit 1
+  fi
+}
+
+# Download and run the Nix installation script
+curl -L $GET_OS_URL -o get_os.sh 
+curl -L $INSTALL_NIX_URL -o install_nix.sh
+chmod +x get_os.sh
+chmod +x install_nix.sh
+./install_nix.sh
+check_exit_status
+
+# Download scripts to run cli
+curl -L $UTILS_URL -o utils.py
+curl -L $TEMPLATES_URL -o templates.py
+curl -L $CLI_URL -o cli.py
+chmod +x cli.py
+./cli.py
+
+# Clean up files
+rm install-nix.sh
+rm get_os.sh
+rm cli.py
+rm utils.py
+rm templates.py
+rm -rf __pycache__
